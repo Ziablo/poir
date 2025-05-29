194dfecd426c03d59fe7e9d2f93531855f7ddd54
Votre Nom <ametdiedhiou52@gmail.com>
2025-05-29 12:05:41 +0200

    feat: ajout de la langue anglaise pour les navigateurs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
+import { ThemeProvider } from '@/components/ui/use-theme'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,15 +18,22 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="fr">
+    <html lang="en">
       <body className={`${inter.className} min-h-screen bg-gradient-to-b from-[rgb(26,26,26)] to-[rgb(45,45,45)]`}>
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <main className="flex-1">
-            {children}
-          </main>
-          <Footer />
-        </div>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="flex-1">
+              {children}
+            </main>
+            <Footer />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   )
